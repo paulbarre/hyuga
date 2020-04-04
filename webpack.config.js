cf76235f98988047a9efd77d822b8897521dcb58
Paul Barré <paul.yvon.barre@gmail.com>
2020-04-04 22:51:17 +0900

    Setup webpack html and clean plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,21 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
 module.exports = {
-    mode: 'development',
-    entry: './src/index.ts',
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader'
-            }
-        ]
-    }
+  mode: 'development',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
 }
