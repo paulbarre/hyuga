219b82ed1135354467d949e707cd6baee7d0b796
Paul Barré <paul.yvon.barre@gmail.com>
2020-04-06 23:07:52 +0900

    Update example

--- examples/vue-hyuga/src/App.vue
+++ examples/vue-hyuga/src/App.vue
@@ -11,8 +11,8 @@ import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
 }
 </script>
 
@@ -24,5 +24,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h-card {
+  --text-color: purple;
 }
 </style>
