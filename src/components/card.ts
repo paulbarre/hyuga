dd79847e240ef02ba2061e7416cdd9348ef1848a
unknown <paul.yvon.barre@gmail.com>
2020-04-06 13:28:48 +0900

    Add style to element

--- src/components/card.ts
+++ src/components/card.ts
@@ -1,8 +1,16 @@
 class HCard extends HTMLElement {
   connectedCallback() {
     const shadow = this.attachShadow({ mode: 'open' })
+
+    const style = document.createElement('style')
+    style.textContent = `
+      div { color: red; }
+    `
+
     const div = document.createElement('div')
     div.innerHTML = 'Bonjour, je suis une carte'
+
+    shadow.appendChild(style)
     shadow.appendChild(div)
   }
 }
